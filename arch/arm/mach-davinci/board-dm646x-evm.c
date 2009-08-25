b5af17e71a652ddef70a348691a2e841b91ea561
Chaithrika U S <chaithrika@ti.com>
2009-08-25 15:20:05 +0300

    davinci: audio: move tlv320aic33 i2c setup into board files
    Tested on DM6446, DM355, DM6447, DA850 EVMs.
    
    Signed-off-by: Chaithrika U S <chaithrika@ti.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-davinci/board-dm646x-evm.c
+++ arch/arm/mach-davinci/board-dm646x-evm.c
@@ -349,6 +349,9 @@ static struct i2c_board_info __initdata i2c_info[] =  {
 	{
 		I2C_BOARD_INFO("cpld_video", 0x3B),
 	},
+	{
+		I2C_BOARD_INFO("tlv320aic33", 0x18),
+	}
 };
 
 static struct davinci_i2c_platform_data i2c_pdata = {
